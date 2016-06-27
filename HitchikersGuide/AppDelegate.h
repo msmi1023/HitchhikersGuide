2d3a0e05adb0289e9f0f01c4e8be5be3f38848ea
msmi1023 <msmi1023@ford.com>
2016-06-27 11:35:03 -0400

    Initial project setup

---
+++ HitchikersGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HitchikersGuide
+//
+//  Created by tstone10 on 6/27/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
